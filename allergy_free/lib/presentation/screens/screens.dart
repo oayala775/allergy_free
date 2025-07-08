fc9c7ce18b1048bbb3ea4073cc12511b70f5db33
omar Linux Victus <oayala775@gmail.com>
2025-07-07 19:33:44 -0600

    Added this file to centralize the calling of screens in order to use only one import

---
+++ allergy_free/lib/presentation/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:allergy_free/presentation/screens/home_screen.dart';
+export 'package:allergy_free/presentation/screens/profile_screen.dart';
+export 'package:allergy_free/presentation/screens/settings_screen.dart';
+export 'package:allergy_free/presentation/screens/transition_screen.dart';
