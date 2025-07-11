f93c2f0995d2524341fadcc66694f3276b7f048b
Omar HP Windows <oayala775@gmail.com>
2025-07-10 19:10:47 -0600

    Added screens inside their own folder

--- allergy_free/lib/presentation/screens/screens.dart
+++ allergy_free/lib/presentation/screens/screens.dart
@@ -1,4 +1,4 @@
-export 'package:allergy_free/presentation/screens/home_screen.dart';
-export 'package:allergy_free/presentation/screens/profile_screen.dart';
-export 'package:allergy_free/presentation/screens/settings_screen.dart';
-export 'package:allergy_free/presentation/screens/transition_screen.dart';
+export 'package:allergy_free/presentation/screens/home/home_screen.dart';
+export 'package:allergy_free/presentation/screens/profile/profile_screen.dart';
+export 'package:allergy_free/presentation/screens/settings/settings_screen.dart';
+export 'package:allergy_free/presentation/screens/transition/transition_screen.dart';
