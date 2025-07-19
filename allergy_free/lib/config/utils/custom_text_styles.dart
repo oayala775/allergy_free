a6b12c292a4a2244def23c7c89693c09c33c362a
omar Linux Victus <oayala775@gmail.com>
2025-07-19 13:30:22 -0600

    Added text styles that are going to be used alongside the app

---
+++ allergy_free/lib/config/utils/custom_text_styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:allergy_free/config/utils/custom_colors.dart';
+
+class CustomTextStyles {
+  static const TextStyle greyedText = TextStyle(
+    color: CustomColors.greyLetters,
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+    fontFamily: "Inter",
+  );
+  static const TextStyle whiteText600 = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    fontFamily: "Inter",
+  );
+  static const TextStyle title = TextStyle(
+    color: CustomColors.subtitle,
+    fontWeight: FontWeight.w600,
+    fontSize: 32,
+    fontFamily: "Inter",
+  );
+  static const TextStyle signaling = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w900,
+    fontSize: 32,
+    fontFamily: "Inter",
+  );
+  static const TextStyle greenLogin = TextStyle(
+    color: CustomColors.primary,
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+    fontFamily: "Inter",
+  );
+}
