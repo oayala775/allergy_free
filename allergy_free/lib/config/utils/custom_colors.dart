1d20442138c69630aabb45577781f98d3f67ee01
omar Linux Victus <oayala775@gmail.com>
2025-07-19 13:29:51 -0600

    Added two more colors

--- allergy_free/lib/config/utils/custom_colors.dart
+++ allergy_free/lib/config/utils/custom_colors.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 class CustomColors {
   static const Color focus = Color(0xFF3B873E);
   static const Color primary = Color.fromRGBO(44, 128, 38, 1);
-  static const Color greyLetters = Color(0xFF7E7E7E);
+  static const Color greyLetters = Color.fromRGBO(126, 126, 126, 1);
   static const Color warningRed = Color.fromRGBO(243, 0, 0, 1);
   static const Color defaultShadow = Color.fromRGBO(0, 0, 0, 0.25);
   static const Color signUpLink = Color.fromRGBO(4, 48, 1, 1);
+  static const Color subtitle = Color.fromRGBO(58, 58, 58, 1);
 }
