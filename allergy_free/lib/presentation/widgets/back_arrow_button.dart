470a146ae6d8bab3e52649d538fdb61c074239ef
omar Linux Victus <oayala775@gmail.com>
2025-08-24 10:50:16 -0600

    Changed back button to rounded

--- allergy_free/lib/presentation/widgets/back_arrow_button.dart
+++ allergy_free/lib/presentation/widgets/back_arrow_button.dart
@@ -13,9 +13,10 @@ class BackArrowButton extends StatelessWidget {
         onPressed: () => context.pop(),
         backgroundColor: CustomColors.primary,
         elevation: 0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
+        shape: CircleBorder(),
+        // shape: RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.all(Radius.circular(10)),
+        // ),
         child: const Icon(
           Icons.arrow_back_ios_new_rounded,
           color: Colors.white,
