aedee66073103e43ec4b4dca9819fa94954b074b
omar Linux Victus <oayala775@gmail.com>
2025-10-12 12:15:42 -0600

    feat: Added new provider for selected allergens that keeps track of the allergens selected

---
+++ allergy_free/lib/presentation/providers/selected_avatar_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/legacy.dart';
+
+final selectedAvatarProvider = StateProvider<String>((ref) =>'assets/images/avatar/0_Default.png');
---
+++ allergy_free/lib/presentation/providers/terms_and_conditions_provider.dart
@@ -0,0 +1,4 @@
+
+import 'package:flutter_riverpod/legacy.dart';
+
+final termsAndConditionsProvider = StateProvider<bool>((ref) => false);
